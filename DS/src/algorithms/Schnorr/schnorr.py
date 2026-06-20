919c9af6331854a10d25491e80083804cccab4c4
boyzest <boyzestsumbayak@gmail.com>
2026-06-20 18:52:06 +0700

    done schnorr

--- DS/src/algorithms/Schnorr/schnorr.py
+++ DS/src/algorithms/Schnorr/schnorr.py
@@ -3,7 +3,7 @@ import hashlib
 import os
 
 
-def keygen (g,x,p):
+def keygen (g,x,p,q):
     y = mth.modularExponentiation(g,x,p)
     print(y)
 
@@ -18,90 +18,90 @@ def keygen (g,x,p):
     return publicKey,privateKey
 
 
-def signing (m,g,k,p,x):
+def signing (m,g,k,p,q,x):
 
     r = mth.modularExponentiation(g,k,p)
 
     m_concat_r = f"{m}{r}"
-    hashValue = int(hashlib.sha1(m_concat_r.encode()).hexdigest()[:4], 16)
-    hashValue = hashValue % q
+    e = int(hashlib.sha256(m_concat_r.encode()).hexdigest(), 16)
+    e = e % q
 
-    s = (k+x*hashValue) % q
+    s = (k+x*e) % q
 
-    signature = (hashValue,s)
+    signature = (e,s)
 
     print (f"signature  = {signature}")
     return signature
 
 
-def verif (m,e,s,g,p,y):
+def verif (m,e,s,g,p,q,y):
     rv = (mth.modularExponentiation(g,s,p) * mth.modularExponentiation(y,e,p)) % p
 
     m_concat_rv = f"{m}{rv}"
-    hashValue = int(hashlib.sha1(m_concat_rv.encode()).hexdigest()[:4], 16)
-    ev = hashValue % q
+    ev = int(hashlib.sha256(m_concat_rv.encode()).hexdigest(), 16)
+    ev = ev % q
 
-    print (f"h1 = {rv}")
-    print (f"h2 = {ev}")
-    print (f"ev = e ? {ev==e}")
+    print(f"rv = {rv}")
+    print(f"ev = {ev}")
+    print(f"e  = {e}")
+    print(f"Valid = {ev == e}")
 
 def clear ():
     os.system('cls')
 
 
-p = 1109
-q = 277
-g = 13
-x = 140
-k = 131
+while True:
+    p = 1109
+    q = 277
+    g = 13
 
-m = "Kriptografi mantap_18"
+    try:
+        x = int(input(f"masukkan nilai x (bilangan acak yang lebih kecil dari {q}-1) : "))
+        k = int(input(f"masukkan nilai k (acak yang kurang dari {q}-1) : "))
+        m = input("Masukkan nilai m : ")
 
-publicKey, privateKey = keygen(g,x,p)
-signature = signing(m,g,k,p,x)
-verif(m,signature[0],signature[1],g,p,publicKey[2])
+        if not (0 < k < q):
+            print(f"Nilai k tidak valid. k harus memenuhi 0 < k < {q}")
+            continue
+
+        if not (0 < x < q):
+            print(f"Nilai x tidak valid. x harus memenuhi 0 < x < {q}")
+            continue
+            
+    except ValueError:
+        print("Input error: Pastikan memasukkan angka yang benar.")
+        continue
 
 
 
-# while True:
-#     m = input("Masukkan nilai m : ")
-#     p = 100003
-#     q = 123457
-#     e = int(input("masukkan nilai e(relatif prima) : "))
+    print()
+    print("="*30)
+    print()
+    print("proses pembentukan key : ")
+    publicKey, privateKey = keygen(g, x, p, q)
+    print()
 
-#     phiN = (p-1)* (q-1)
-#     while True:
-#         if mth.gcd(e, phiN) != 1:
-#             print(f"{e} tidak relatif prima terhadap φ(n) = {phiN}")
-#             e = int(input("masukkan nilai e(relatif prima) : "))
-#         else:
-#             break 
-#     print()
-#     print("="*30)
-#     print()
-#     print("proses pembentukan key : ")
-#     n,phiN,d,publicKey,privateKey = keygen(p,q,e)
-#     print()
+    input("tekan enter untuk melanjutkan proses sign !")
 
-#     input("tekan enter untuk melanjutkan proses sign !")
+    print()
+    print("="*30)
+    print()
+    print("proses sign : ")
+    signature = signing(m, g, k, p, q, x)
+    print()
 
-#     print()
-#     print("="*30)
-#     print()
-#     print("proses sign : ")
-#     hashValue, signature =  signing(m,n,d)
-#     print()
+    input("tekan enter untuk melanjutkan proses verify !")
 
-#     input("tekan enter untuk melanjutkan proses verify !")
+    print()
+    print("="*30)
+    print()
+    print("proses verify : ")
+    verif(m, signature[0], signature[1], g, p, q, publicKey[2])
+    print()
 
-#     print()
-#     print("="*30)
-#     print()
-#     print("proses verify : ")
-#     verif(hashValue)
+    confirmation = input("tekan enter untuk kembali atau isi dengan char apapun untuk mengakhiri: ")
 
-#     confirmation = input("tekan enter untuk kembali atau isi dengan char untuk mengakhiri")
-
-#     if confirmation != "":
-#         break
-#     clear()
+    if confirmation != "":
+        break
+        
+    clear()
